12a9f7eb9d3112a142c6ecb0f6960081d2303b94
atrial874 <95119887+atrial874@users.noreply.github.com>
2022-05-03 07:04:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -7,7 +7,7 @@ class script(object):
 ✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/filesharebotusers>AML UPDATES</a>"""
     
     ABOUT_TXT = """<b>⌬ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-⌬ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/NL_MP4>𝙽𝙸𝙷𝙰𝙰𝙻</a>
+⌬ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/https://filesharebotusers>𝚃𝙴𝙰𝙼 𝙰𝙼𝙻</a>
 ⌬ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ⌬ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ⌬ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
